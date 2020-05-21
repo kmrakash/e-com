776dc5ce2a7d5373dfe9aa3ff2eeb23713afdbb7
Akash Kumar <aksh.iem22@gmail.com>
2020-05-21 16:24:34 +0530

    styling and components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from './Home.component';
+import Home from './components/pages/homepage/Home.component';
 import './App.css';
 
 function App() {
---
+++ src/components/pages/homepage/Home.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../directory/directory.components';
+
+
+const Home = () => (
+    <div  className="homepage">
+        <Directory />
+    </div>
+);
+
+export default Home;
